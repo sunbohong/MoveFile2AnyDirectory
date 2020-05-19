08377d584293b447db120d1fc725ba27dfc4c3e3
unknown <>
2020-05-20 00:13:04 +0800

    Publish 0.1 version

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "movefile2anydirectory" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	console.log('Congratulations, your extension "MoveFile2AnyDirectory" is now active!');
+	{
+		// The command has been defined in the package.json file
+		// Now provide the implementation of the command with registerCommand
+		// The commandId parameter must match the command field in package.json
+		for (let i = 1; i <= 7; i++) {
+			const command = 'com.sun.MoveFile2AnyDirectory' + i;
+			let disposable = vscode.commands.registerCommand(command, () => {
+				const targetConfig = vscode.workspace.getConfiguration().get<string>(command);
+				if (!targetConfig) {
+					vscode.window.showErrorMessage('MoveFile2AnyDirectory:Undefined config');
+					return;
+				}
+				const getDestPath = () => {
+					if (targetConfig.includes(':')) {
+						const filePath = vscode.window.activeTextEditor!.document.fileName;
+						const destPath = targetConfig + '\\' + (filePath.split('\\').reverse()[0]);
+						return destPath;
+					} else {
+						const rootPath = vscode.workspace.rootPath;
+						const filePath = vscode.window.activeTextEditor!.document.fileName;
+						const destPath = rootPath + '\\' + targetConfig + '\\' + (filePath.split('\\').reverse()[0]);
+						return destPath;
+					}
+				};
+				const destPath = getDestPath();
+				vscode.workspace.fs.rename(vscode.Uri.file(vscode.window.activeTextEditor!.document.fileName), vscode.Uri.file(destPath));
+				vscode.window.showInformationMessage(destPath);
+			});
+			context.subscriptions.push(disposable);
+		}
+	}
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
